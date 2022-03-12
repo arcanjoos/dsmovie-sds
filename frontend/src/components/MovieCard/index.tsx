06cb1ad793ee83d0b4682acfd07dec0809abf44a
arcanjoos <zebustudios@gmail.com>
2022-03-12 07:13:28 -0300

    mostrar estrelinhas

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 
 type Props = {
-    movie : Movie
+    movie: Movie
 }
 
 
-function MovieCard({movie}: Props) {
+function MovieCard({ movie }: Props) {
 
     return (
         <div>
             <img className='dsmovie-movie-card-image' src={movie.image} alt={movie.title} />
             <div className='dsmovie-card-bottom-container'>
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.count} />
                 <Link to={`form/${movie.id}`}>
                     <div className='btn btn-primary dsmovie-btn'>Avaliar</div>
                 </Link>
